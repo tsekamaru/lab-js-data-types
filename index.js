f950614293ef04da3b026b33337a9cd3f6a1027d
terde2 <tseka.nice@gmail.com>
2024-10-16 15:16:12 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister = ""
+tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4
 
 // Print out the concatenated string
-
+console.log(tongueTwister)
 
 
 
@@ -22,10 +23,20 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+let result = "";
+let result1 = "";
+let result2 = "";
+result1 = part1.slice(0,part1.length-1);
+result1 = part1[part1.length-1].toUpperCase();
+result1 = part1.slice(0,part1.length-1) + part1[part1.length-1].toUpperCase();
 
+result2 = part2.slice(0,part2.length-1);
+result2 = part2[part2.length-1].toUpperCase();
+result2 = part2.slice(0,part2.length-1) + part2[part2.length-1].toUpperCase();
 
+result = result1 + result2
 // Print the cameLtaiL-formatted string
-
+console.log(result)
 
 
 
@@ -35,10 +46,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,10 +58,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let randomNumber = Math.floor(Math.random()*10)
 
 // Print the generated random number
-
+console.log(randomNumber)
 
 
 /*******************************************
@@ -62,15 +73,24 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
+
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
